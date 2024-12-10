8dfe2ba0755f2d3de6d206a489fb41e26920be1a
Ivan Khabarov <ivan@ihabarov.ru>
2024-12-10 09:14:13 +0300

    added new features

--- recipes_site/recipes_site/wsgi.py
+++ recipes_site/recipes_site/wsgi.py
@@ -7,10 +7,25 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+# This file contains the WSGI configuration required to serve up your
+# web application at http://IvanHabarov17.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Django project
+
 import os
+import sys
 
+# add your project directory to the sys.path
+project_home = '/home/IvanHabarov17/mysite'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+
+# serve django via WSGI
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipes_site.settings')
-
 application = get_wsgi_application()
