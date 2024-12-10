e1546e897e0cfd2a13fb499aa92616842226a2ac
Ivan Khabarov <ivan@ihabarov.ru>
2024-12-10 12:19:32 +0300

    added new features2

--- recipes_site/recipes_site/settings.py
+++ recipes_site/recipes_site/settings.py
@@ -119,10 +119,13 @@ USE_I18N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -169,3 +172,6 @@ AUTH_USER_MODEL = "recipes.CustomUser"  # для кастомной модели
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+MEDIA_ROOT = BASE_DIR / 'media'
